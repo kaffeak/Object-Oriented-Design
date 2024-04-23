ce63617cd2bd8123a8730bd4dc6ad4bead2ebdd6
Pontus Hector <tjeckizz@gmail.com>
2024-04-23 20:42:13 +0200

    Fixed EIS + FakeDB
    ExternalInventorySystem was added in practice

--- Sem3/Integration/ExternalInventorySystem.java
+++ Sem3/Integration/ExternalInventorySystem.java
@@ -1,13 +1,32 @@
 package Integration;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.List;
 
+import Integration.ItemDTO;
+
 public class ExternalInventorySystem {
-    private ItemDTO item;
-    
-    private List<ItemDTO> invetnoryItems = {{1, "test", 12387f, 0.87f}, };
-    
+    private ItemDTO itemDTO;
+    private List<ItemDTO> inventoryItems = new ArrayList<>();
+
     public ExternalInventorySystem() {
+        try {
+            File inventory = new File("Integration/inventoryItems.txt");
+            Scanner scanner = new Scanner(inventory);
+            String[] info;
+            int i = 0;
+            while (scanner.hasNextLine()) {
+                info = scanner.nextLine().split(" ");
+                itemDTO = new ItemDTO(Integer.parseInt(info[0]), info[1], Float.parseFloat(info[2]), Float.parseFloat(info[3]));
+                inventoryItems.add(itemDTO);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("An error occurred.");
+            ex.printStackTrace();
+        }
     }
 
     public void updateInventory(List<ItemDTO> items){
@@ -19,6 +38,16 @@ public class ExternalInventorySystem {
     public ItemDTO getItem(int ItemID){
         //Extract item information from external inventory system with provided ID
         //Return THIS item
+        return inventoryItems.get(ItemID);
     }
+    /*
+    public static void main(String args[]) {
+        ExternalInventorySystem ex = new ExternalInventorySystem();
+        System.out.println(ex.getItem(0).getName());
+        System.out.println(ex.getItem(1).getName());
+        System.out.println(ex.getItem(2).getName());
+        System.out.println(ex.getItem(3).getName());
+        System.out.println(ex.getItem(4).getName());
 
+    }*/
 }
