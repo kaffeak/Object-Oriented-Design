6e67c2209fdea7fcafc56d417464d1f07ad25d4d
Simon Malmqvist Dichy <dichy@kth.se>
2024-04-23 14:58:18 +0200

    Update to controller

--- Sem3/Controller/Controller.java
+++ Sem3/Controller/Controller.java
@@ -1,21 +1,41 @@
-package Sem3.Controller;
+package Controller;
 
+import Model.AddToSaleInfo;
+import Model.Sale;
+import Integration.DiscountDB;
+import Integration.ItemDTO;
 
 public class Controller {
+    private ExternalAccountingSystem accountingSystem;
+    private ExternalInventorySystem inventorySystem;
+    private ReceiptPrinter printer;
+    private DiscountDB discount;
+    private Sale sale;
+    private View view;
+    private AddToSaleInfo saleInfo;
 
     Controller cont = new Controller();
+    
+    public Controller(Sale sale, View view, ExternalAccountingSystem accountingSystem, ExternalInventorySystem inventorySystem, ReceiptPrinter printer, DiscountDB discount){
+        this.sale = sale;
+        this.discount = discount;
+        this.accountingSystem = accountingSystem;
+        this.inventorySystem = inventorySystem;
+        this.printer = printer;
+        this.view = view;
+    }
+    
     public void startSale(){
-        Sale sale = new Sale();
-
+        sale = new Sale();
     }
-    public void addItem(){
-
-    }
-    public float endSale(){
+    public void addItem(int quantity, int itemID){
         
     }
-    public float retrieveDiscount(){
-
+    public float endSale(){
+        return sale.getRunningTotal();
+    }
+    public float retrieveDiscount(int customerId){
+        return sale.get;
     }
     public float getChange(){
 
