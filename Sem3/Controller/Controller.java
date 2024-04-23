dc7a753bad1eb8f7b4308266346393e3428c240f
Simon Malmqvist Dichy <dichy@kth.se>
2024-04-23 13:56:09 +0200

    Created Controller.java

---
+++ Sem3/Controller/Controller.java
@@ -0,0 +1,24 @@
+package Sem3.Controller;
+
+
+public class Controller {
+
+    Controller cont = new Controller();
+    public void startSale(){
+        Sale sale = new Sale();
+
+    }
+    public void addItem(){
+
+    }
+    public float endSale(){
+        
+    }
+    public float retrieveDiscount(){
+
+    }
+    public float getChange(){
+
+    }
+    
+}
